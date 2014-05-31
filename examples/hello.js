7d2683ae157fa62a99563d2aaafaad5593635a08
NathanGRomano <nathan.g.romano@gmail.com>
2014-05-31 13:55:11 -0400

    fixed issue with double biding of event handlers

--- examples/hello.js
+++ examples/hello.js
@@ -1,14 +1,12 @@
 var bus = require('./..')();
 bus.listen(8080);
 bus.on('say', function (message) {
-  console.log('the message', message);
   message.deliver(); 
 });
 
 setTimeout(function () {
   var client = require('socket.io-client')('http://localhost:8080');
   client.on('connect', function () {
-    console.log('connected');
     client.emit('say','Hello');
   });
   client.on('say', function (message) {
