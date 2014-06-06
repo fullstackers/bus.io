57427e9808c89f018b960821d7e33f43f4046f0d
NathanGRomano <nathan.g.romano@gmail.com>
2014-06-05 20:05:09 -0400

    prepend instead of append, documentation, added TODO's, and updated to use new message interface

--- demo/chat/app.js
+++ demo/chat/app.js
@@ -11,52 +11,6 @@ bus.actor(function (socket, cb) {
   cb(null, socket.name || socket.id);
 });
 
-// we want to use the last argument as the "target" only if we have more than one argument
-bus.target(function (socket, params, cb) {
-  if (params.length > 1) {
-    cb(null, params.pop());
-  }
-  else {
-    cb(null, socket.name || socket.id)
-  }
-});
-
-// when we receive connection, we want to alias the socket to everyone
-bus.socket(function (socket, bus) {
-  bus.alias(socket, 'everyone');
-});
-
-// when we receive a message from the socket we want chop it down to 128 characters
-bus.in(function (message, socket, next) {
-  var content = message.data.content[0];
-  if (content && content.length > 128) {
-    message.data.content[0] = content.slice(0,125)+'...';
-  }
-  next();
-});
-
-// when we get a post message we want to deliver
-bus.on('post', function (message) {
-  message.deliver();
-});
-
-// when we get a "set user" we want to deliver it
-bus.on('set user', function (message) {
-  message.deliver();
-});
-
-// when we receive the "set user" message we want to alias the socket to the user
-bus.out(function (message, socket, next) {
-  switch(message.data.action) {
-    case 'set user': {
-      var name = message.data.content[0];
-      bus.alias(socket, name);
-      socket.name = name;
-    } break;
-  }
-  next();
-});
-
 /** 
  * TODO Instead of using actor I would like to use a middlware function
  */
@@ -68,6 +22,16 @@ bus.in(function (message, socket, next) {
 })
 */
 
+// we want to use the last argument as the "target" only if we have more than one argument
+bus.target(function (socket, params, cb) {
+  if (params.length > 1) {
+    cb(null, params.pop());
+  }
+  else {
+    cb(null, socket.name || socket.id)
+  }
+});
+
 /**
  * TODO Instead of using target I would like to use a middleware function
  */
@@ -78,3 +42,70 @@ bus.in(function (message, socket, next) {
   next();
 });
 */
+
+// when we receive connection, we want to alias the socket to everyone
+bus.socket(function (socket, bus) {
+  bus.alias(socket, 'everyone');
+});
+
+/*
+ * TODO would be nice if we could o
+
+bus.in('post', function (message, socket, next) {
+  if (message.content().length && message.content()[0].length > 128) {
+    message.content(message.content().shift().slice(0,125)+'...');
+  }
+  next();
+});
+
+*/
+
+// when we receive a message from the socket we want chop it down to 128 characters
+bus.in(function (message, socket, next) {
+  if (message.content().length && message.content()[0].length > 128) {
+    message.content(message.content().shift().slice(0,125)+'...');
+  }
+  next();
+});
+
+/*
+ * TODO would be nice if we could do
+
+bus.autoPropagate(true);
+
+*/
+
+// when we get a post message we want to deliver
+bus.on('post', function (message) {
+  message.deliver();
+});
+
+// when we get a "set user" we want to deliver it
+bus.on('set user', function (message) {
+  message.deliver();
+});
+
+/*
+ * TODO Would be nice if we could handle messages like
+
+bus.out('set user', function (message, socket, next) {
+  var name = message.content()[0];
+  bus.alias(socket, name);
+  socket.name = name;
+  next();
+});
+
+*/
+
+// when we receive the "set user" message we want to alias the socket to the user
+bus.out(function (message, socket, next) {
+  switch(message.action()) {
+    case 'set user': {
+      var name = message.content()[0];
+      bus.alias(socket, name);
+      socket.name = name;
+    } break;
+  }
+  next();
+});
+
