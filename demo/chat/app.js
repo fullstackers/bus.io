e35f530c30625a089ccf4bcce9863f160c2bd47a
NathanGRomano <nathan.g.romano@gmail.com>
2014-06-08 03:25:00 -0400

    updated the demo code

--- demo/chat/app.js
+++ demo/chat/app.js
@@ -1,8 +1,11 @@
-var cluster = require('cluster');
+/*
+ * Use Cluster to fork this process same number of times as we have cpus!
+ */
+
+var cluster = require('cluster'), cpus = require('os').cpus().length;
 
 if (cluster.isMaster) {
 
-  var cpus = require('os').cpus().length || 1;
   for (var i=0; i<cpus; i++) {
     cluster.fork();
   }
@@ -12,114 +15,95 @@ if (cluster.isMaster) {
   return;
 }
 
+/*
+ * We are a child process now
+ */
+
+/*
+ * Get Express up and running
+ */
+
 var app = require('express')();
 app.get('/', function (req, res) {
   res.sendfile(__dirname+'/public/index.html');
 });
 
-var server = require('http').Server(app).listen(80);
+/*
+ * Create a Server to attach our Express app
+ */
 
-// we want to use the "socket.name" or the "socket.id" as the actor
+var server = require('http').Server(app).listen(process.env.PORT || 3000);
+
+/*
+ * Get a Bus and using our Server
+ */
+
 var bus = require('bus.io')(server);
-bus.actor(function (socket, cb) {
-  cb(null, socket.name || socket.id);
-});
-
-/** 
- * TODO Instead of using actor I would like to use a middlware function
- */
 
 /*
-bus.in(function (message, socket, next) {
-  message.actor(socket.user);
-  next();
-})
-*/
-
-// we want to use the last argument as the "target" only if we have more than one argument
-bus.target(function (socket, params, cb) {
-  if (params.length > 1) {
-    cb(null, params.pop());
-  }
-  else {
-    cb(null, socket.name || socket.id)
-  }
-});
-
-/**
- * TODO Instead of using target I would like to use a middleware function
+ * We want our socket to receive messages when sent to everyone
  */
 
-/*
-bus.in(function (message, socket, next) {
-  message.target(message.content().pop());
-  next();
-});
-*/
-
-// when we receive connection, we want to alias the socket to everyone
 bus.socket(function (socket, bus) {
   bus.alias(socket, 'everyone');
 });
 
 /*
- * TODO would be nice if we could o
+ * For all messages we will set the message actor to either the socket.name or 
+ * socket.id
+ */
 
-bus.in('post', function (message, socket, next) {
-  if (message.content().length && message.content()[0].length > 128) {
-    message.content(message.content().shift().slice(0,125)+'...');
-  }
-  next();
-});
-
-*/
-
-// when we receive a message from the socket we want chop it down to 128 characters
 bus.in(function (message, socket, next) {
-  if (message.content().length && message.content()[0].length > 128) {
-    message.content(message.content().shift().slice(0,125)+'...');
-  }
+  message.actor(socket.name || socket.id);
   next();
 });
 
 /*
- * TODO would be nice if we could do
+ * When we receive a "set name" event from the socket store that on the socket
+ * and deliver() the message to the Exchange.
+ */
 
-bus.autoPropagate(true);
+bus.in('set name', function (message, socket) {
+  socket.name = message.content();
+  message.deliver();
+});
 
-*/
 
-// when we get a post message we want to deliver
+/*
+ * When we receive a "post" event from the socket, cap the content length to
+ * 128 characters.
+ */
+
+bus.in('post', function (message, socket) {
+  message.target(message.content().pop());
+  if (message.content().length && message.content()[0].length > 128) {
+    message.content(message.content().slice(0,125)+'...');
+  }
+  message.deliver();
+});
+
+/*
+ * When the Bus finnaly gets the "post" message just deliver to the target.
+ */
+
 bus.on('post', function (message) {
   message.deliver();
 });
 
-// when we get a "set user" we want to deliver it
-bus.on('set user', function (message) {
+/*
+ * When the Bus finally gets the "set name" message just deliver to the target.
+ */
+
+bus.on('set name', function (message) {
   message.deliver();
 });
 
 /*
- * TODO Would be nice if we could handle messages like
+ * Before we deliver the "set name" event to the socekt,  alias the socket to 
+ * the "name".
+ */
 
-bus.out('set user', function (message, socket, next) {
-  var name = message.content()[0];
-  bus.alias(socket, name);
-  socket.name = name;
+bus.out('set name', function (message, socket, next) {
+  bus.alias(socket, message.content());
   next();
 });
-
-*/
-
-// when we receive the "set user" message we want to alias the socket to the user
-bus.out(function (message, socket, next) {
-  switch(message.action()) {
-    case 'set user': {
-      var name = message.content()[0];
-      bus.alias(socket, name);
-      socket.name = name;
-    } break;
-  }
-  next();
-});
-
