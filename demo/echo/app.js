deeef621056edd36ba7db9c157fbda300c37ad7b
NathanGRomano <nathan.g.romano@gmail.com>
2014-06-08 22:26:26 -0400

    aadded echo demo

---
+++ demo/echo/app.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var server = require('http').createServer(app).listen(process.env.PORT || 3000);
+var bus = require('bus.io')(server);
