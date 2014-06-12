1bb3ddf2ae8b71a97541c0ee301517c9a6773844
NathanGRomano <nathan.g.romano@gmail.com>
2014-06-12 01:43:48 -0400

    chaned to Server

--- demo/echo/app.js
+++ demo/echo/app.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-var server = require('http').createServer(app).listen(process.env.PORT || 3000);
+var server = require('http').Server(app).listen(process.env.PORT || 3000);
 var bus = require('bus.io')(server);
